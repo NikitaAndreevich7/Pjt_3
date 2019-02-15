1e173cb400478f5fc2120375cfb2850ac0d9904a
NikitaAndreevich7 <programmist-97@mail.ru>
2019-02-16 01:42:39 +0300

    and head PC

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// (function($){
+//     $(function(){
+//         $('.icon').on('click', function(){
+//             $(this).closest('.menu').toggleClass('menu-open');
+//         });
+//     });
+// })(jQuery);
+
+// function slowScroll(id){
+//     var offset = 0;
+//     $('html,body').animate({
+//         scrollTop: $(id).offset ().top - offset
+//     }, 700);
+//     return false;
+// }

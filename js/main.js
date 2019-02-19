2130ba1f0b3f48fe8c2a1ae0c90ec381f855f71f
NikitaAndreevich7 <programmist-97@mail.ru>
2019-02-19 03:09:27 +0300

    Гамбургер меню

--- js/main.js
+++ js/main.js
@@ -1,15 +1,56 @@
-// (function($){
-//     $(function(){
-//         $('.icon').on('click', function(){
-//             $(this).closest('.menu').toggleClass('menu-open');
-//         });
-//     });
-// })(jQuery);
+function burgerMenu(selector){
+    let menu = $(selector);
+    let button = menu.find('.burger-menu__button');
+    let links = menu.find('burger-menu__link');
+    let overlay = menu.find('.burger-menu__overlay');
 
-// function slowScroll(id){
-//     var offset = 0;
-//     $('html,body').animate({
-//         scrollTop: $(id).offset ().top - offset
-//     }, 700);
-//     return false;
-// }
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+    links.on('click',() => toggleMenu())
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu(){
+        menu.toggleClass('burger-menu_active');
+
+        if(menu.hasClass('burger-menu_active')){
+            $('body').css('overflow', 'hidden');
+        }else{
+            $('body').css('overflow', 'visible');
+        }
+    }
+}
+burgerMenu('.burger-menu');
+
+function slowScroll(id){
+    var offset = 0;
+    $('html,body').animate({
+        scrollTop: $(id).offset ().top - offset
+    }, 700);
+    return false;
+}
+
+
+
+$(document).ready(function() { // вся мaгия пoсле зaгрузки стрaницы
+	$('a#go').click( function(event){ // лoвим клик пo ссылки с id="go"
+		event.preventDefault(); // выключaем стaндaртную рoль элементa
+		$('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
+		 	function(){ // пoсле выпoлнения предъидущей aнимaции
+				$('#modal_form') 
+					.css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
+					.animate({opacity: 1, top: '50%'}, 200); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
+		});
+	});
+	/* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+	$('#modal_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
+		$('#modal_form')
+			.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
+				function(){ // пoсле aнимaции
+					$(this).css('display', 'none'); // делaем ему display: none;
+					$('#overlay').fadeOut(400); // скрывaем пoдлoжку
+				}
+			);
+	});
+});

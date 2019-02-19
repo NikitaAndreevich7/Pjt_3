2608603e7d409c55d3f343316cc95b7fd37f8b9b
NikitaAndreevich7 <programmist-97@mail.ru>
2019-02-20 02:42:55 +0300

    Кнопка якорь,которая возвращает на главную

--- js/main.js
+++ js/main.js
@@ -23,9 +23,22 @@ function slowScroll(id){
     return false;
 }
 
+// button
 
+$('body').append('<button class="btn_up"/>');
 
+$('.btn_up').click(function(){
+    $('body').animate({'scrollTop':0}, 1000);
+    $('html').animate({'scrollTop':0}, 1000);
+});
 
+$(window).scroll(function(){
+    if($(window).scrollTop() > 200){
+        $('.btn_up').addClass('active');
+    }else{
+        $('.btn_up').removeClass('active');
+    }
+})
 // модальные окна
 
 
